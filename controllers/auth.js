f96d5d4fd15bca328600ad2e5f6bed37d7747fed
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-01-10 01:18:43 +0300

    feat: add registration with bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+
 module.exports.login = function(req, res) {
   res.status(200).json({
     login: {
@@ -8,8 +12,27 @@ module.exports.login = function(req, res) {
 }
 
 
-module.exports.register = function(req, res) {
-  res.status(200).json({
-    register: 'from controller'
-  })
+module.exports.register = async function(req, res) {
+  const candidate = await User.findOne({email: req.body.email})
+
+  if (candidate) {
+    res.status(409).json({
+      message: 'User with this email exist'
+    })
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    })
+
+    try {
+      await user.save()
+      res.status(201).json(user)
+    } catch(e) {
+     
+    }
+
+  }
 }
