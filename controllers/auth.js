117fa65b8a311d59e7937b2fdebed62de68db1bc
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-01-10 02:08:16 +0300

    feat: add autorization with token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,34 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
+const tokenLifeTime = 3600 //one hour
 
-module.exports.login = function(req, res) {
+module.exports.login = async function(req, res) {
+  const candidate = await User.findOne({email: req.body.email})
+
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: tokenLifeTime}) 
+
   res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        message: 'Invalid password'
+      })
     }
+  } else {
+    res.status(404).json({
+      message: 'User with such email wasn`t found'
   })
+}
 }
 
 
